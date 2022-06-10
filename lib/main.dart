c241fc4300166095f01a1a4e67e75f47c4d7a74d
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2022-06-10 21:42:10 +0700

    build: :wrench: update some configurations
    * add launch config for debug & run (vscode)
    * change title of the app

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Doet',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.teal,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Doet'),
     );
   }
 }
